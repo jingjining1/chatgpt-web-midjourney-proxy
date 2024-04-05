9557f651ff89cba1b0f8032375254dd55c5b8669
jingjining1 <133858747+jingjining1@users.noreply.github.com>
2024-04-05 23:31:55 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,22 +1,24 @@
-const {
-    createProxyMiddleware
-} = require('http-proxy-middleware')
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
-    let target = ''
-    let headers= {}
+    let target = '';
+    let headers = {};
+
     // 代理目标地址
-    if (req.url.startsWith('/mjapi')) { //这里使用/api可能会与vercel serverless 的 api 路径冲突，根据接口进行调整
-        target = process.env.MJ_SERVER??'https://api.openai.com';
-        headers= {
+    if (req.url.startsWith('/mjapi')) {
+        // 这里使用/api可能会与vercel serverless的api路径冲突，根据接口进行调整
+        target = process.env.MJ_SERVER ?? 'https://api.openai.com';
+        headers = {
             'Mj-Api-Secret': process.env.MJ_API_SECRET // 添加自定义请求头
-        }
-    }else if(req.url.startsWith('/openapi')){
-        target = process.env.OPENAI_API_BASE_URL??'https://api.openai.com';
-        headers= {
-            'Authorization': 'Bearer ' +process.env.OPENAI_API_KEY // 添加自定义请求头
-        }
+        };
+    } else {
+        // 自定义的URL
+        target = 'https://asia.neuroapi.host';
+        headers = {
+            'Authorization': 'Bearer ' + process.env.OPENAI_API_KEY // 添加自定义请求头
+        };
     }
+
     // 创建代理对象并转发请求
     createProxyMiddleware({
         target,
@@ -24,8 +26,9 @@ module.exports = (req, res) => {
         headers,
         pathRewrite: {
             // 通过路径重写，去除请求路径中的 `/api`
-            '^/mjapi/': '/'
-            ,'^/openapi/': '/'
+            '^/mjapi/': '/',
+            '^/openapi/': '/'
         }
-    })(req, res)
-}
+    })(req, res);
+};
+
